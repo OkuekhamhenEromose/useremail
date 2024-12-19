6f8c6e84456c43dc88abf76875ba0a8c4d4976a7
Charles Eromose <okuekhamhene.gmail.com>
2024-12-19 08:29:45 +0100

    class profile

--- api/settings.py
+++ api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i$!82yr26gr(oc=76igcw1w#t2!wah%x&lm-)0-x$*ee_*4^sf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True#False(Always put to False when pushing projects online)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []#['localhost','127.0.0.1:8000']
 
 
 # Application definition
--- useremailapps/models.py
+++ useremailapps/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+GENDER = (
+    ('M','Male'),
+    ('F','Feale'),
+)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    fullname = models.CharField(max_length=255)
+    phone = models.ImageField(max_length=50)
+    gender = models.CharField(max_length=10, choices=GENDER)
+    profile_pix = models.ImageField(upload_to='profile', blank=True, null=True)#you must install pillow to use imagefield=pip install pillow
+
+    def __str__(self):
+        return self.fullname
+
